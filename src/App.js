069b4bbe808eac747957c7211c87143ab5a17663
VenkataSrikanthG <srikanth.techops@gmail.com>
2020-02-17 23:03:58 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi Chitti Pandu, you're 5 years old. soon you are going to be 6.
+          Happy Family Day from Amma and Daddy
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
